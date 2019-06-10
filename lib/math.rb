a5e2dc7879b9c381ce70b4b9e935ec6ad4379f13
Gregory Thomas <gregorythomasjr55@gmail.com>
2019-06-10 17:11:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(5, 4)
+  5 + 4
 end
 
 def subtraction(10, 5)
+  10 - 5
 end
 
 def division(50, 2)
+  50 / 2
 end
 
 def multiplication(4, 30)
+  4 * 30
 end
 
 def modulo(34, 5)
+  34 % 5
 end
 
 def square_root(81)
+  Math.sqrt(81)
 end
 
-def order_of_operation(7, 43, 23, 83
+def order_of_operation(7, 43, 23, 83)
+  7 + 43/23 - 83
  
 end
