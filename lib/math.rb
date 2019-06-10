0bb6fca019440b4a3c83a74ecf0c7ed381e630b2
Gregory Thomas <gregorythomasjr55@gmail.com>
2019-06-10 17:12:10 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
-def addition(num1, num2)
+def addition(5, 4)
+  5 + 4
 end
 
-def subtraction(num1, num2)
+def subtraction(10, 5)
+  10 - 5
 end
 
-def division(num1, num2)
+def division(50, 2)
+  50 / 2
 end
 
-def multiplication(num1, num2)
+def multiplication(4, 30)
+  4 * 30
 end
 
-def modulo(num1, num2)
+def modulo(34, 5)
+  34 % 5
 end
 
-def square_root(num)
+def square_root(81)
+  Math.sqrt(81)
 end
 
-def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+def order_of_operation(7, 43, 23, 83)
+  7 + 43/23 - 83
+ 
 end
